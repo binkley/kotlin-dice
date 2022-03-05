d479a9b57d083f3958390afbd3bb155777438f91
Brian Oxley <boxley@thoughtworks.com>
2022-03-05 16:38:54 -0600

    Improve demo

--- src/main/kotlin/hm/binkley/dice/main.kt
+++ src/main/kotlin/hm/binkley/dice/main.kt
@@ -48,48 +48,41 @@ fun exceptionsFor(options: Options) =
  * The second value is the expectation given a seed of 1 used in testing.
  */
 val demoExpressions = arrayOf(
-    "d6" to 4,
+    "d6" to 4, // standard die
     " d6" to 4, // whitespace
     "d6 " to 4, // whitespace
     " d6 " to 4, // whitespace
-    "D6" to 4,
-    "d6x2" to 8,
-    "z6" to 3,
-    "Z6" to 3,
-    "z6x2" to 6,
-    "1d1" to 1,
-    "1z1" to 0,
-    "3d6" to 10,
-    "3D6" to 10,
-    "1d1" to 1, // check bounds
-    "1z1" to 0, // check bounds
+    "D6" to 4, // case-insensitive
+    "z6" to 3, // zero-based die
+    "Z6" to 3, // case-insensitive
+    "d%" to 66, // percentile die
+    "3d6" to 10, // multiple dice
+    "1d1" to 1, // bounds
+    "1z1" to 0, //  bounds
+    "d6x2" to 8, // doubling
+    "d6X2" to 8, // case-insensitive
+    "d6*2" to 8, // synonym
+    "3d6 +2d4" to 17, // whitespace
+    "3d6+ 2d4" to 17, // whitespace
+    "3d6 + 2d4" to 17, // whitespace
     "3d6+1" to 11, // adding adjustment
     "3d6+ 1" to 11, // whitespace
     "3d6 +1" to 11, // whitespace
     "3d6 + 1" to 11, // whitespace
     "3d6x2+1" to 21, // double before adding one
     "3d6-1" to 9, // subtracting adjustment
-    "10d3!" to 20,
-    "10d3!*2" to 40, // double after exploding
-    "10d3!x2" to 40, // synonym for times
-    "10d3!X2" to 40, // synonym for times
+    "4d6h3" to 10, // keep 3 highest rolls
+    "4d6H3" to 10, // case-insensitive
+    "4d6l3" to 6, // keep 3 lowest rolls
+    "4d6L3" to 6, // case-insensitive
+    "10d3!" to 20, // explode
+    "10d3!x2" to 40, // double after exploding
     "10d3!2" to 49,
-    "4d6h3" to 10,
-    "4d6H3" to 10,
-    "4d6l3" to 6,
-    "4d6L3" to 6,
-    "3d6+2d4" to 17,
-    "3d6 +2d4" to 17, // whitespace
-    "3d6+ 2d4" to 17, // whitespace
-    "3d6 + 2d4" to 17, // whitespace
-    "d%" to 66,
-    // Constant seed keeps the roll constant, so z% is one less than d%
-    "z%" to 65,
     "6d4l5!" to 20,
     "3d3r1h2!" to 10,
     "3d12!10" to 23,
-    "100d3r1h99!+100d3r1l99!3-17" to 919,
-    "blah" to null,
+    "100d3r1h99!+100d3r1l99!3-17" to 919, // complex
+    "blah" to null, // syntax error
 )
 
 private fun maybeGnuPrefix(): String {
