d9fe9a51061ebe1fed484e276ed26e09774c7d58
Brian Oxley <boxley@thoughtworks.com>
2022-03-14 10:39:47 -0500

    Naming

--- src/main/kotlin/hm/binkley/dice/ReplRoller.kt
+++ src/main/kotlin/hm/binkley/dice/ReplRoller.kt
@@ -5,7 +5,33 @@ import hm.binkley.dice.NeedsLineReader.DoNeedsLineReader
 import hm.binkley.dice.NeedsSystemRegistry.DoNeedsSystemRegistry
 import lombok.Generated
 import org.jline.console.impl.SystemRegistryImpl.UnknownCommandException
+import org.jline.reader.LineReader
+import org.jline.terminal.Terminal
 import kotlin.random.Random
+
+class OldReplRoller(
+    random: Random,
+    reporter: MainReporter,
+    private val terminal: Terminal,
+    private val options: Options,
+) : MainRoller(random, reporter) {
+    private val lineReader: LineReader = options.oldLineReader(terminal)
+
+    /** Note: closes (and resets) the terminal when done. */
+    override fun rollAndReport() = terminal.use {
+        while (true) try {
+            // TODO: Untested, and @Generated does compile for lambdas
+            rollFromLines { lineReader.readLine(options.prompt) }
+        } catch (e: HistoryException) {
+            e.printError()
+        } catch (e: DiceException) {
+            e.printError()
+        }
+    }
+
+    private fun Throwable.printError() =
+        options.commandLine.err.println(colorScheme.errorText(message))
+}
 
 @Generated
 class NewReplRoller(
